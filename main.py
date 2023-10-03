73b5a1593a72b7a5407d48a333a0362740a7add8
25ejsb <eitan.brochstein@icloud.com>
2023-10-02 21:37:04 -0400

    lol

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, url_for, flash, jsonify
+from datetime import datetime, timedelta
+import pandas as pd
+app = Flask(__name__)
+
+data = pd.read_csv("data.csv")
+
+@app.route('/')
+def home():
+  return render_template("index.html", data=data)
+
+@app.route("/gettime")
+def gettime():
+    time = datetime.now().strftime("%A, %B %d, %Y, %I:%M:%S %p")
+    return jsonify({"Content": time})
+app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ static/main.js
@@ -0,0 +1,15 @@
+$(async function() {
+    response = await fetch("/gettime")
+    let promise = Promise.resolve(response.json())
+    promise.then(data => {
+        document.querySelector("main h2").textContent = data['Content']
+    })
+})
+setInterval(async function() {
+    response = await fetch("/gettime")
+    let promise = Promise.resolve(response.json())
+    promise.then(data => {
+        document.querySelector("main h2").textContent = data['Content']
+    })
+    
+}, 1000);
