3e8b703e4809e32b0468a366312820d184bf51ea
25ejsb <eitan.brochstein@icloud.com>
2024-02-13 21:49:28 -0500

    lol

--- process.py
+++
@@ -1,7 +0,0 @@
-import subprocess, signal
-subprocess.Popen(['python3', 'app.py'],
-                 stdin=subprocess.DEVNULL,
-                 stdout=open('nohup.out', 'w'),
-                 stderr=subprocess.STDOUT,
-                 start_new_session=True,
-                 preexec_fn=(lambda: signal.signal(signal.SIGHUP, signal.SIG_IGN)))
